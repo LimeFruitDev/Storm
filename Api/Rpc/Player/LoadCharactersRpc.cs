81db8765632799311d027b6e30ac89c3226f0f74
SleepyMode <sleepy@sleepymode.me>
2023-07-27 14:30:49 +0300

    api: Create LoadCharacters Rpc

---
+++ Api/Rpc/Player/LoadCharactersRpc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Storm;
+
+public class LoadCharactersRpc : BaseRpc
+{
+	public LoadCharactersRpc(Player player)
+	{
+		MessageType = "loadCharacters";
+		PlayerId = player.UniqueId;
+	}
+
+	[JsonPropertyName("playerId")] public Guid PlayerId { get; set; }
+}
