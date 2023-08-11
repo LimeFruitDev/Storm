0ae244f77e67b864290796b305ecfff56043954c
SleepyMode <sleepy@sleepymode.me>
2023-08-11 14:32:48 +0300

    api: Create the SaveItem RPC

--- Api/Rpc/ItemSystem/SaveItemRpc.cs
+++ Api/Rpc/ItemSystem/SaveItemRpc.cs
@@ -1,6 +1,20 @@
-﻿namespace Storm;
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
-// TODO
+namespace Storm;
+
 public class SaveItemRpc : BaseRpc
 {
+	public SaveItemRpc(ItemInstance item)
+	{
+		MessageType = "saveItem";
+		UniqueId = item.UniqueId;
+		InventoryId = item.InventoryId;
+		Properties = JsonSerializer.Serialize(item.Data.Properties);
+	}
+
+	[JsonPropertyName("uniqueId")] public Guid UniqueId { get; set; }
+	[JsonPropertyName("inventoryId")] public Guid InventoryId { get; set; }
+	[JsonPropertyName("properties")] public string Properties { get; set; }
 }
