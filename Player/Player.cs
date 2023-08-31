665562c964ebd21095431e468e4d3f1911491d34
SleepyMode <sleepy@sleepymode.me>
2023-08-31 18:35:42 +0300

    player: Use squared brackets in verbose name for uniqueid

--- Player/Player.cs
+++ Player/Player.cs
@@ -23,5 +23,5 @@ public partial class Player : AnimatedEntity
 	public string SteamName => Client.Name;
 	public string SteamId => Client.SteamId.ToString();
 	public bool IsAlive => LifeState == LifeState.Alive;
-	public string VerboseName => $"{SteamName}({SteamId})<{UniqueId}>";
+	public string VerboseName => $"{SteamName}({SteamId})[{UniqueId}]";
 }
